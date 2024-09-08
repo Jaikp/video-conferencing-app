6c856916a49a7e39225b42cabeebfa91bc097849
Jaikp <jaikp000@gmail.com>
2024-09-09 03:00:12 +0530

    file and folder structure

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarlinks = [
+    {
+        label: 'Home',
+        route:'/',
+        imgUrl: '/icons/Home.svg',
+    },
+    {
+        label: 'upcoming',
+        route:'/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+    {
+        label: 'Previous',
+        route:'/previous',
+        imgUrl: '/icons/Previous.svg',
+    },
+    {
+        label: 'Recordings',
+        route:'/recordings',
+        imgUrl: '/icons/Video.svg',
+    },
+    {
+        label: 'Personal Room',
+        route:'/personal-r0om',
+        imgUrl: '/icons/add-personal.svg',
+    },
+
+]
